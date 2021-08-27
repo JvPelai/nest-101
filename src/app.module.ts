b80d2c74ed4363997da9138c93a9c47acf14367d
JvPelai <jv.pelai@gmail.com>
2021-08-27 18:24:29 -0300

    basic controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminController } from './controllers/admin.controller';
+import { CatsController } from './controllers/cats.controller';
+import { DocsController } from './controllers/docs.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, CatsController, DocsController, AdminController],
   providers: [AppService],
 })
 export class AppModule {}
