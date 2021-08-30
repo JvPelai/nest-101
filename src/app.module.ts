7b56c5a4afe58e338294d71a67150d799e60a3b0
JvPelai <jv.pelai@gmail.com>
2021-08-30 20:30:31 -0300

    guards and interceptors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_PIPE } from '@nestjs/core';
 import { CatsController } from './cats/cats.controller';
 import { CatsModule } from './cats/cats.module';
 import { HttpExceptionFilter } from './errors/http-exception.filter';
+import { RolesGuard } from './guards/roles.guard';
 import { logger } from './middlewares/logger.middleware';
 import { ValidationPipe } from './pipes/validation.pipe';
 
@@ -16,6 +17,10 @@ import { ValidationPipe } from './pipes/validation.pipe';
     {
       provide: APP_PIPE,
       useClass: ValidationPipe,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
     },
   ],
 })
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  SetMetadata,
+  UseInterceptors,
+} from '@nestjs/common';
 import { CatsService } from 'src/cats/cats.service';
 import { CreateCatDto } from 'src/cats/dtos/create-cat.dto';
 import { ForbiddenException } from 'src/errors/forbidden.exception';
+import { LoggingInterceptor } from 'src/interceptors/logging.interceptor';
 import { Cat } from './interfaces/cat.interface';
 
 @Controller('cats')
+@UseInterceptors(LoggingInterceptor)
 export class CatsController {
   constructor(private catsService: CatsService) {}
 
   @Post()
+  @SetMetadata('roles', ['admin'])
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
   }
